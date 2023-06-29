f0572d10b03b882eba78714247d30d5729944e16
luizsant <luizsant@gmail.com>
2023-06-29 18:05:39 -0300

    enviao o exercicio

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1>Calculadora Aritmética</h1>
+    <Calculadora />
+  </div>
+</template>
+
+<script>
+import Calculadora from "./components/Calculadora.vue";
+
+export default {
+  components: {
+    Calculadora
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 400px;
+  margin: 0 auto;
+  text-align: center;
+  padding: 20px;
+}
+</style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="calculadora">
+    <input type="number" v-model="numero1" @input="calcularResultado" />
+    <select v-model="operacao" @change="calcularResultado">
+      <option value="+">+</option>
+      <option value="-">-</option>
+      <option value="*">*</option>
+      <option value="/">/</option>
+    </select>
+    <input type="number" v-model="numero2" @input="calcularResultado" />
+    <h2>Resultado: {{ resultado }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numero1: 0,
+      numero2: 0,
+      operacao: "+",
+      resultado: 0
+    };
+  },
+  methods: {
+    calcularResultado() {
+      const num1 = parseFloat(this.numero1);
+      const num2 = parseFloat(this.numero2);
+
+      switch (this.operacao) {
+        case "+":
+          this.resultado = num1 + num2;
+          break;
+        case "-":
+          this.resultado = num1 - num2;
+          break;
+        case "*":
+          this.resultado = num1 * num2;
+          break;
+        case "/":
+          this.resultado = num1 / num2;
+          break;
+        default:
+          this.resultado = 0;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.calculadora {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+}
+
+input,
+select {
+  margin-bottom: 10px;
+  padding: 5px;
+}
+</style>
